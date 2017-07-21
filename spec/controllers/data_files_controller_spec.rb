61793ce6c55d1d9fa7f579a27a4c357f7958a571
Kelly Munro <kelly.munro@powershop.co.nz>
2017-07-21 15:09:32 +1200

    Use fixture_file_upload in the data_files_controller spec

--- spec/controllers/data_files_controller_spec.rb
+++ spec/controllers/data_files_controller_spec.rb
@@ -2,19 +2,15 @@ require 'rails_helper'
 
 RSpec.describe DataFilesController, type: :controller do
   describe '#upload' do
-    let(:data_file) { double('test') }
+    let(:data_file) { fixture_file_upload('sample_data.txt', 'text/txt') }
     let!(:plug) { Plug.create!(name: 'Test Plug', login_user: 'admin', login_password: '1234', ip_address: '127.0.0.1') }
 
-    before do
-      allow(data_file).to receive(:tempfile).and_return('5/19/2017, 3:04:50 PM;11;40')
+    it 'creates a data_file' do
+      expect { post :upload, params: { id: plug.id, data_file: data_file } }.to change { DataFile.count }.by 1
     end
 
-    xit 'creates a data_file' do
-      expect { post :upload, params: { id: plug.id, data_file: { data_file: data_file } } }.to change { DataFile.count }.by 1
-    end
-
-    xit 'redirects to the plug details screen' do
-      post :upload, params: { id: plug.id, data_file: { data_file: data_file } }
+    it 'redirects to the plug details screen' do
+      post :upload, params: { id: plug.id, data_file: data_file }
 
       expect(response).to redirect_to plug
     end
