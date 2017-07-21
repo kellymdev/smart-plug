33364d472cef347f5f3f2270bcc11a8d59a01817
Kelly Munro <kelly.munro@powershop.co.nz>
2017-07-21 14:23:40 +1200

    Consumption data route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :plugs
 
   post '/plugs/:id/upload', to: 'data_files#upload'
+
+  get '/plugs/:plug_id/consumption_data', to: 'graphs#consumption_data'
 end
