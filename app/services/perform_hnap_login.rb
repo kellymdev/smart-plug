0160323d78c68986a285767df33807d62a47cb27
Kelly Munro <kelly.munro@powershop.co.nz>
2017-08-18 14:38:44 +1200

    Initial code for performing login request

--- app/services/perform_hnap_login.rb
+++ app/services/perform_hnap_login.rb
@@ -1,15 +1,21 @@
 class PerformHnapLogin
   attr_reader :plug, :hnap_login
 
+  BASE_URL = 'http://purenetworks.com/HNAP1/'
+  LOGIN_URL = "#{base_url}Login"
+  LOGIN_RESULT_URL = "#{base_url}LoginResult"
+
   def initialize(plug)
     @plug = plug
     @hnap_login = plug.hnap_logins.new
   end
 
   def call
-    response = HTTParty.post(plug_url, headers: login_headers, body: login_body)
+    response = initial_request
 
     parse_xml(response.body)
+
+    perform_login
   end
 
   private
@@ -21,7 +27,7 @@ class PerformHnapLogin
   def login_headers
     {
       'Content-Type' => 'text/xml; charset=utf-8',
-      'SOAPAction' => 'http://purenetworks.com/HNAP1/Login'
+      'SOAPAction' => LOGIN_URL
     }
   end
 
@@ -30,7 +36,7 @@ class PerformHnapLogin
 <?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope "xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
-    <Login xmlns="http://purenetworks.com/HNAP1/">
+    <Login xmlns=#{BASE_URL}>
       <Action>request</Action>
       <Username>#{plug.login_user}</Username>
       <LoginPassword></LoginPassword>
@@ -39,6 +45,10 @@ class PerformHnapLogin
   </soap:Body>
 </soap:Envelope>
     XML
+  end
+
+  def initial_request
+    HTTParty.post(plug_url, headers: login_headers, body: login_body)
   end
 
   def parse_xml(response)
@@ -58,6 +68,45 @@ class PerformHnapLogin
   end
 
   def private_key
+    # TODO: HMAC_MD5 this
     "#{hnap_login.public_key} #{plug.login_password} #{hnap_login.challenge}".upcase
+  end
+
+  def perform_login
+    HTTParty.post(plug_url, headers: login_result_headers, body: login_result_body)
+  end
+
+  def login_result_headers
+    {
+      'Content-Type' => 'text/xml; charset=utf-8',
+      'SOAPAction' => LOGIN_RESULT_URL,
+      'HNAP_AUTH' => hnap_auth,
+      'Cookie' => "uid=#{hnap_login.cookie}"
+    }
+  end
+
+  def hnap_auth
+    time_stamp = Time.now
+    auth = "#{private_key} #{time_stamp} #{LOGIN_RESULT_URL}" # TODO: HMAC_MD5 this
+
+    "#{auth.upcase} #{time_stamp}"
+  end
+
+  def login_result_body
+    password = "#{hnap_login.private_key}#{hnap_login.challenge}" # TODO: HMAC_MD5 this
+
+    <<-XML
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope "xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <LoginResult xmlns=#{BASE_URL}>
+      <Action>login</Action>
+      <Username>#{plug.login_user}</Username>"
+      <LoginPassword>#{password.upcase}</LoginPassword>
+      <Captcha></Captcha>
+    </LoginResult>
+  </soap:Body>
+</soap:Envelope>
+    XML
   end
 end
